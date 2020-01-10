6a48e6cda20b10b5600335e94c6ee0ac6e16165f
Flowing <bitoreieru@gmail.com>
2020-01-11 03:38:01 +0900

    added testing

---
+++ challonge_test.go
@@ -0,0 +1,31 @@
+package challonge_test
+
+import (
+	"github.com/FlowingSPDG/go-challonge"
+	"testing"
+)
+
+const (
+	// Get API key from https://challonge.com/ja/settings/developer
+	User string = ""
+	Key  string = ""
+)
+
+func TestNew(t *testing.T) {
+	client := challonge.New(User, Key)
+	tournament, err := client.NewTournamentRequest("sample_tournament_1").Get()
+
+	if err != nil {
+		t.Fatalf("unable to retrieve tournament.\nERR : %v\n", err)
+	}
+	t.Logf("Tournament : %v\n", tournament)
+	t.Logf("Tournament name: %s\n", tournament.Name)
+	t.Logf("Tournament desc: %s\n", tournament.Description)
+
+	matches := tournament.GetMatches()
+	t.Logf("Matches : %v\n", matches)
+
+	participant := tournament.Participants
+	t.Logf("Participant : %v\n", participant)
+
+}
